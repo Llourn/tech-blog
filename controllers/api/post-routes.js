b8d85db367221fa82758e3b59f70e33a469cc356
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-20 11:27:03 -0300

    added routes placeholders and started on models

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+// Get all posts for user
+
+// Get post by id
+
+// Update post by id
+
+// Delete post
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// Render home page
+
+// Render login/signup page
+
+// Render blog post
+
+// Render User Profile
+
+module.exports = router;
